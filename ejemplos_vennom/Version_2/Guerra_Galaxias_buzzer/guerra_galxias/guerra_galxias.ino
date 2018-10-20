e45da7b40f8c14701150e7e2d0631eb270c3b486
Juan Sanmartín <j.andrs300@gmail.com>
2018-10-19 19:42:24 -0500

    Guerra de la galxias
    Tonos de guerra de las galaxias en Arduino y Scratch, usando MBlock.
    Programación para niños.

---
+++ ejemplos_vennom/Version_2/Guerra_Galaxias_buzzer/guerra_galxias/guerra_galxias.ino
@@ -0,0 +1,46 @@
+void setup(){
+    pinMode(11,OUTPUT);
+}
+
+void loop(){
+    
+    tone(11,349,500); // write to buzzer
+    delay(500);
+    tone(11,349,500); // write to buzzer
+    delay(500);
+    tone(11,349,500); // write to buzzer
+    delay(500);
+    tone(11,294,250); // write to buzzer
+    delay(250);
+    tone(11,440,250); // write to buzzer
+    delay(250);
+    tone(11,349,500); // write to buzzer
+    delay(500);
+    tone(11,294,250); // write to buzzer
+    delay(250);
+    tone(11,440,250); // write to buzzer
+    delay(250);
+    tone(11,349,500); // write to buzzer
+    delay(500);
+    delay(500);
+    tone(11,523,500); // write to buzzer
+    delay(500);
+    tone(11,523,500); // write to buzzer
+    delay(500);
+    tone(11,523,500); // write to buzzer
+    delay(500);
+    tone(11,523,250); // write to buzzer
+    delay(250);
+    tone(11,440,250); // write to buzzer
+    delay(250);
+    tone(11,349,500); // write to buzzer
+    delay(500);
+    tone(11,294,250); // write to buzzer
+    delay(250);
+    tone(11,440,250); // write to buzzer
+    delay(250);
+    tone(11,349,500); // write to buzzer
+    delay(500);
+    delay(500);
+
+}
